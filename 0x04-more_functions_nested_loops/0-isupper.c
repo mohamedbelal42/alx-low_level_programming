a2159335765bb775cb1f642cc4474612fbf90e4c
mohamedbelal42 <hamooo12345x@gmail.com>
2023-09-14 15:10:42 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _isuper - check if c is upper
+ * _isupper - check if c is upper
  *
  * @c: input for alphabet
  *
  * Return: 1 if uppercase, 0 if not
+ *
 */
 
 int _isupper(int c)
